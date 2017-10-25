c6bc43f6ebb07c0ee400753c4a160364197a811e
Matthew McDonald <matthew.mcd1031@gmail.com>
2017-10-25 04:28:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function add(a,b){
-  return a + b 
+  return a + b
 }
 function subtract(a,b){
   return a - b
@@ -16,3 +16,6 @@ function inc(a){
 function dec(a){
   return a -= 1
 }
+function makeInt(a){
+  parseInt(a)
+} 
